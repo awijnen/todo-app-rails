2e79d49b566d849d647687cb9ae72e225550d25b
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-02-28 17:43:21 -0500

    Revert "first commit"
    This reverts commit b27467255c246e6f32d1c60c372f333588eed32b.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ToDoApp::Application.config.secret_token = 'cf6a72bdc58f632b985b99384d139e25bfcbcac8869dc8540794698eb97737138d3a7edf48b3aafac22ddd96510deb51fca1269ffb62c6b90e655f0e24384e67'
